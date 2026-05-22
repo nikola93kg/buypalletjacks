33eae9f603c8f7480eea65519a933abe3acf1fe8
Nikola Markovic <markovic755@gmail.com>
2026-05-22 22:59:39 +0200

    vercel link redirection

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [75, 85],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "buypalletjacks.vercel.app" }],
+        destination: "https://www.buypalletjacks.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
